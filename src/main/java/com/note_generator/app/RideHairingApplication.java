9c3e7e6da8d2a1c4a0fdaab3512e714609e8c7ec
daniela1409 <ydaniela.osorio@udea.edu.co>
2022-10-20 20:51:58 -0500

    Configuracion inicial del proyecto

---
+++ src/main/java/com/note_generator/app/RideHairingApplication.java
@@ -0,0 +1,13 @@
+package com.note_generator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideHairingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideHairingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/note_generator/app/RideHairingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note_generator.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideHairingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
